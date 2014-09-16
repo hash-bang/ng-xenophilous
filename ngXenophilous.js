4b090c8b73c77b5633291a5cf4747ddc75da15a9
Matt Carter <m@ttcarter.com>
2014-09-16 12:32:28 +1000

    Better error handling in weird edge cases

--- ngXenophilous.js
+++ ngXenophilous.js
@@ -5,11 +5,17 @@
 * @param function cb The function to execute
 */
 function ngApply(controller, cb) {
-	var controller = $("[ng-controller='" + controller + "']");
-	if (!controller)
+	var controllerObj = $("[ng-controller='" + controller + "']");
+	if (!controllerObj.length)
 		return console.error('Cannot find controller', controller);
 
-	var scope = controller.scope();
+	var angularObj = angular.element(controllerObj);
+	if (!angularObj || !angularObj.scope)
+		return console.error('Found controller', controller, 'but Angular didnt return a matching internal object!');
+
+	var scope = angularObj.scope();
+	if (!scope)
+		return console.error('Angular controller', controller, 'exists but it doesnt seem to have a scope!');
 
 	scope.$apply(function() {
 		cb(scope);
